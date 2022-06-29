c3ae8f782560bc0691537b76fc5657a4cebfe0f6
Siddhesh Jungade <32316726+siddheshJungade@users.noreply.github.com>
2022-06-29 21:33:44 +0530

    update email id

--- src/Component/Social/Social.js
+++ src/Component/Social/Social.js
@@ -5,7 +5,7 @@ const Social = (props) => {
     return (
         <div className="social">
             <div className="social__item">
-                <a href='mailto:siddheshjungade@gmail.com' target="_blank" rel="noreferrer" ><i className="far fa-envelope fa-2x"></i></a>
+                <a href='mailto:connect@siddheshjungade.me' target="_blank" rel="noreferrer" ><i className="far fa-envelope fa-2x"></i></a>
                 <a href='https://twitter.com/siddheshjungade' target="_blank" rel="noreferrer"><i className="fab fa-twitter fa-2x"></i></a>
                 <a href='https://github.com/siddheshJungade' target="_blank" rel="noreferrer"><i className="fab fa-github-alt fa-2x"></i></a>
                 <a href='https://www.linkedin.com/in/siddhesh-jungade/' target='_black' rel="noreferrer"><i className="fab fa-linkedin fa-2x"></i></a>
