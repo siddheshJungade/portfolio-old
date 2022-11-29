e212d9c9ff66de2e24de5ce5c4d594517a45dd03
Siddhesh Jungade <32316726+siddheshJungade@users.noreply.github.com>
2022-11-29 08:58:56 +0530

    Update Social.js

--- src/Component/Social/Social.js
+++ src/Component/Social/Social.js
@@ -8,8 +8,8 @@ const Social = (props) => {
                 <a href='mailto:connect@siddheshjungade.me' target="_blank" rel="noreferrer" ><i className="far fa-envelope fa-2x"></i></a>
                 <a href='https://twitter.com/siddheshjungade' target="_blank" rel="noreferrer"><i className="fab fa-twitter fa-2x"></i></a>
                 <a href='https://github.com/siddheshJungade' target="_blank" rel="noreferrer"><i className="fab fa-github-alt fa-2x"></i></a>
-                <a href='https://www.linkedin.com/in/siddhesh-jungade/' target='_black' rel="noreferrer"><i className="fab fa-linkedin fa-2x"></i></a>
-                <a href='https://brothepro.hashnode.dev/' target='_blank' rel="noreferrer"><i className="fas fa-blog fa-2x"></i></a>
+                <a href='https://www.linkedin.com/in/siddheshjungade/' target='_black' rel="noreferrer"><i className="fab fa-linkedin fa-2x"></i></a>
+                <a href='https://blog.siddheshjungade.me' target='_blank' rel="noreferrer"><i className="fas fa-blog fa-2x"></i></a>
                 <a href='https://www.instagram.com/siddheshjungade/' target='_blank'  rel="noreferrer"><i className="fab fa-instagram fa-2x"></i></a>
             </div>
         </div>
